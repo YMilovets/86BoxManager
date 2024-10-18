1445a52d8097e866c3f55fe5feadee6a3af05c50
YMilovets <y.milovets@yandex.ru>
2024-10-18 20:50:10 +0700

    Инициализация electron-приложения и настроек vite-сборщика проекта 86BoxManager

---
+++ index.js
@@ -0,0 +1,69 @@
+import { ipcMain, app as App, dialog } from "electron";
+import { join } from "path";
+
+import { Window } from "./lib/Window.js";
+import { readdirSync } from "fs";
+import { exec } from "child_process";
+import { format } from "url";
+
+const ROOT_DIR = `${App.getPath('home')}//.86Box`;
+const lockInstance = App.requestSingleInstanceLock();
+if (!lockInstance) {
+  App.quit();
+}
+let isMachineStarted = false;
+
+function main() {
+  const mainWindow = new Window({
+    file: format({
+      pathname: join(App.getAppPath(), "index.html"),
+      protocol: "file:",
+      slashes: true,
+    }),
+    webPreferences: {
+      preload: join(App.getAppPath(), "preload.cjs"),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false,
+    },
+  });
+  mainWindow.setMenu(null);
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on("close", (e) => {
+    if (isMachineStarted) {
+      e.preventDefault();
+      dialog.showMessageBox({
+        type: "info",
+        buttons: ["OK"],
+        message:
+          "Для закрытия окна программы выключите все запущенные виртуальные машины",
+      });
+    }
+    return false;
+  });
+}
+
+App.on("ready", main);
+
+App.on("second-instance", () => {
+  return false;
+});
+
+ipcMain.on("get-init", (e) => {
+  const listConfigFolders = readdirSync(ROOT_DIR, { withFileTypes: true })
+    .filter((d) => d.isDirectory())
+    .map((d) => d.name);
+  e.reply("get-config-machines", listConfigFolders);
+});
+
+ipcMain.on("invoke-machine", (e, machineId) => {
+  isMachineStarted = true;
+
+  exec(`cd ${ROOT_DIR}/${machineId} && 86Box`, (error, stdout) => {
+    if (stdout) {
+      e.reply("unlocked-machine", machineId);
+      isMachineStarted = false;
+    }
+  });
+});
---
+++ preload.cjs
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  getInit: () => ipcRenderer.send("get-init"),
+  onConfigMachines: (callback) =>
+    ipcRenderer.on("get-config-machines", (_, args) => callback(args)),
+  onUnlockedConfiguration: (callback) =>
+    ipcRenderer.on("unlocked-machine", (_, configurationId) =>
+      callback(configurationId)
+    ),
+  invokeMachine: (machineId) => ipcRenderer.send("invoke-machine", machineId),
+});
