ed1f4e535308bfd098beef254428d8db6a052d67
YMilovets <y.milovets@yandex.ru>
2025-06-17 00:00:12 +0700

    Добавлены основные константы и перечисления и применены статусы ошибок в исключениях

---
+++ shared/index.js
@@ -0,0 +1,14 @@
+export const RELEASE_YEAR = 2025;
+export const TAB_KEY = "Tab";
+export const AUTHOR_NAME = "NEXU";
+export const DEFAULT_FOLDER = ".86Box";
+
+export const ErrorType = {
+  IsExistMachine: "0x000",
+  MissingConfiguration: "0x001",
+  NoExistMachineFolder: "0x002",
+  NoExistSpecificFolderAndMachine: "0x003",
+  NoExistSpecificFolder: "0x004",
+  NoCompareConfiguration: "0x005",
+  IsBlockProcess: "0x006",
+};
