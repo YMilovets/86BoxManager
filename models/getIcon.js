759c118a8ca6bcc74ed1e92e814415ff5725877b
YMilovets <y.milovets@yandex.ru>
2025-06-17 11:47:28 +0700

    Добавлена иконка программы исполняемого файла для операционных систем Windows и на ядре Linux

---
+++ models/getIcon.js
@@ -0,0 +1,11 @@
+import getOSPlatform from "./getOSPlatform.js";
+
+export default function getIcon() {
+  const iconList = {
+    linux: "linux/256.png",
+    win32: "win/window.ico",
+  };
+  const typeOS = getOSPlatform();
+
+  return iconList[typeOS] ?? iconList.win32;
+}
