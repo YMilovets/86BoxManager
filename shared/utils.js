f06d294b750475c4d117ddcae1cb050ad995b367
YMilovets <y.milovets@yandex.ru>
2025-08-10 18:30:04 +0700

    Исправлена ошибка обращения к языковым пакетам в директории app.asar.unpacked

--- shared/utils.js
+++ shared/utils.js
@@ -1,3 +1,13 @@
+import { app as App } from "electron";
+
 export function fixLocalizationButton(...labelParams) {
   return labelParams?.map((label) => [label, " "].join("")) ?? [];
+}
+
+export function redirectAsarUnpackedFiles(path) {
+  if (App.isPackaged) {
+    return path.replace("app.asar", "app.asar.unpacked");
+  }
+
+  return path;
 }
