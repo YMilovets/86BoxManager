6c79d3791a518df02f1dd5e99378027a6f88d51c
YMilovets <y.milovets@yandex.ru>
2025-10-07 17:01:30 +0700

    Добавлены плагины автосортировки импортов и преобразование commonjs в модули для shared

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
@@ -10,11 +10,29 @@ module.exports = {
   ignorePatterns: ["dist", ".eslintrc.cjs", "preload.cjs", "forge.config.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "simple-import-sort"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          ["^react", "^@?\\w"],
+          ["^(@|Components)(/.*|$)"],
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          ["^.+\\.?(css)$"],
+        ],
+      },
+    ],
+    "no-unused-vars": [
+      "error",
+      {
+        destructuredArrayIgnorePattern: "^_",
+      },
     ],
   },
 };
