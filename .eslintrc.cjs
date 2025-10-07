31cd7f2ff7ea4503d7b4dd7a90c2c9e81b2749fe
YMilovets <y.milovets@yandex.ru>
2025-10-07 17:01:30 +0700

    Добавлены плагины автосортировки импортов и преобразование commonjs в модули для shared

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,38 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'forge.config.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "preload.cjs", "forge.config.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "simple-import-sort"],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          ["^react", "^@?\\w"],
+          ["^(@|Components)(/.*|$)"],
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          ["^.+\\.?(css)$"],
+        ],
+      },
+    ],
+    "no-unused-vars": [
+      "error",
+      {
+        destructuredArrayIgnorePattern: "^_",
+      },
     ],
   },
 }
