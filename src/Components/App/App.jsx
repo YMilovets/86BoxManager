836f0b885788bd8f836f13c5fa2b59ecca410daa
YMilovets <y.milovets@yandex.ru>
2026-04-20 02:19:20 +0700

    Реализованы провайдеры машин и языка в App и применены контейнеры форм настройки и MainContainer

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,139 +1,30 @@
-import { useEffect, useReducer, useState } from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Outlet, Route, Routes } from "react-router-dom";
 
 import PageAddMachine from "../../Pages/PageAddMachine";
 import PageConfig from "../../Pages/PageConfig";
 import PageMain from "../../Pages/PageMain";
-import { LanguageList } from "../../Shared";
-
-import { DictionaryContext, MachineContext } from "./context";
-import { reducerListMachines } from "./reducers";
+import { LanguageProvider } from "../../Providers/LanguageProvider";
+import { MachineProvider } from "../../Providers/MachineProvider";
 
 import styles from "./App.module.css";
 
 function App() {
-  const [lang, setLang] = useState();
-  const [configLang, setConfigLang] = useState();
-  const [isExistFolder, setIsExistFolder] = useState(false);
-  
-  const [
-    {
-      machineList: listMachinesState,
-      isEdit,
-      prevPathMachines,
-      isStartedMachines,
-    },
-    dispatch,
-  ] = useReducer(reducerListMachines, {
-    machineList: [],
-    isEdit: false,
-    isStartedMachines: false,
-  });
-
-  const { electronAPI } = window;
-
-  async function initLanguage() {
-    const localLanguage = localStorage.getItem("language");
-
-    try {
-      await electronAPI?.changeLanguage({
-        language: localLanguage,
-        isSelected: true,
-      });
-    } catch {
-      localStorage.setItem("language", LanguageList.RU);
-    }
-    await electronAPI?.setConfigLanguage();
-  }
-
-  useEffect(() => {
-    initLanguage();
-
-    electronAPI?.onSetLanguage(({ language, dictionary }) => {
-      setLang('');
-      setTimeout(() => setLang(language), 0);
-      setConfigLang(dictionary);
-      localStorage.setItem("language", language);
-    });
-
-    electronAPI?.onConfigMachines(
-      ({ resultList, activeMachines = new Map() }) => {
-        const rootDirMachines = localStorage.getItem("rootDirMachines");
-        const activeMachinesByFolder =
-          activeMachines.get(rootDirMachines) ?? new Set();
-
-        dispatch({
-          type: "writeMachines",
-          payload: { resultList, activeMachinesByFolder, activeMachines },
-        });
-        dispatch({ type: "setPathMachines", payload: rootDirMachines });
-      }
-    );
-  }, []);
-
-
-  async function getExistFolder() {
-    const isExistPath = await electronAPI?.existFolder(
-      localStorage.getItem("rootDirMachines")
-    );
-    setIsExistFolder(isExistPath);
-    return isExistPath;
-  }
-
   return (
-    <MachineContext.Provider
-      value={{
-        isEdit,
-        isExistFolder,
-        prevPathMachines,
-        listMachines: listMachinesState,
-        isStartedMachines,
-        setStartMachine: (startMachineId) =>
-          dispatch({ type: "startMachine", payload: startMachineId }),
-        removeMachine: (removeMachineId) =>
-          dispatch({ type: "removeMachine", payload: removeMachineId }),
-        setNewMachineName: (machineName, newMachineName) =>
-          dispatch({
-            type: "renameMachine",
-            payload: { machineName, newMachineName },
-          }),
-        unlockMachine: ({
-          isExistFolder,
-          closedMachine,
-          activeMachines,
-          processPathConfiguration,
-        }) =>
-          dispatch({
-            type: "unlockMachine",
-            payload: {
-              isExistFolder,
-              closedMachine,
-              activeMachines,
-              processPathConfiguration,
-            },
-          }),
-        getExistFolder,
-        setIsEdit: (isEditPayload) =>
-          dispatch({ type: "changeStatus", payload: isEditPayload }),
-      }}
-    >
-      <DictionaryContext.Provider
-        value={{
-          dictionary: configLang,
-          language: lang,
-        }}
-      >
+    <MachineProvider>
+      <LanguageProvider>
         <section className={styles.page}>
           <HashRouter>
             <Routes>
-              <Route path="/" element={<PageMain />} />
-              <Route path="/add-machine" element={<PageAddMachine />} />
-              <Route path="/settings" element={<PageConfig />} />
+              <Route path="/" element={<Outlet />}>
+                <Route path="" element={<PageMain />} />
+                <Route path="add-machine" element={<PageAddMachine />} />
+                <Route path="settings" element={<PageConfig />} />
+              </Route>
             </Routes>
           </HashRouter>
         </section>
-      </DictionaryContext.Provider>
-    </MachineContext.Provider>
+      </LanguageProvider>
+    </MachineProvider>
   );
 }
 
--- src/Pages/PageConfig/PageConfig.jsx
+++ src/Pages/PageConfig/PageConfig.jsx
@@ -1,100 +1,20 @@
-import { useCallback, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
 
-import {
-  DictionaryContext,
-  MachineContext,
-} from "../../Components/App/context";
 import Button from "../../Components/Button";
-import InputBrowserFolder from "../../Components/InputBrowserFolder";
+import CancelContainer from "../../Containers/CancelContainer";
+import ConfigFormContainer from "../../Containers/ConfigFormContainer";
 import LanguageContainer from "../../Containers/LanguageContainer";
 import MessageContainer from "../../Containers/MessageContainer";
+import { DictionaryContext } from "../../Providers/LanguageProvider";
 import { getDictionary } from "../../Shared";
-
-import { getDefaultListFormInput } from "./utils";
 
 import styles from "./PageConfig.module.css";
 
 function PageConfig() {
-  const navigate = useNavigate();
-  const [errorMsg, setErrorMsg] = useState("");
-
-  const { dictionary, language } = useContext(DictionaryContext);
-  const { setIsEdit } = useContext(MachineContext);
+  const { dictionary } = useContext(DictionaryContext);
   const { electronAPI } = window;
 
   const getTransition = getDictionary(dictionary);
-
-  const handleChangeMachineName = () => {
-    setErrorMsg("");
-  };
-
-  const handleCancel = () => {
-    setIsEdit(false);
-    navigate("/");
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const preferenceItems = Object.fromEntries(new FormData(e.currentTarget));
-
-    const self = e.currentTarget.elements;
-    try {
-      if (!preferenceItems.destinationAppFolder.trim()) {
-        self.destinationAppFolder.focus();
-        throw new Error(getTransition("errorDestinationAppFolder"));
-      }
-      if (!preferenceItems.configFolder.trim()) {
-        self.configFolder.focus();
-        throw new Error(getTransition("errorConfigFolder"));
-      }
-      const isExistFolder = await window.electronAPI?.existFolder(
-        preferenceItems.configFolder
-      );
-      if (!isExistFolder) {
-        self.configFolder.focus();
-        throw new Error(getTransition("noExistFolder"));
-      }
-      localStorage.setItem("appPath", preferenceItems.destinationAppFolder);
-      localStorage.setItem("rootDirMachines", preferenceItems.configFolder);
-      localStorage.setItem("language", language);
-
-      handleCancel();
-    } catch ({ message }) {
-      setErrorMsg(message);
-    }
-  };
-
-  const handleOpenFileDialog = useCallback(
-    ({ dialogId, dialogType }) =>
-      async () => {
-        try {
-          const path = await window.electronAPI?.openFileDialog(dialogType);
-          setListFormInput((prevListForm) => ({
-            ...prevListForm,
-            [dialogId]: path,
-          }));
-          handleChangeMachineName();
-        } catch {
-          /* empty */
-        }
-      },
-    []
-  );
-
-  const handleChangeInput = useCallback((e) => {
-    setListFormInput((prevListForm) => ({
-      ...prevListForm,
-      [e.target.id]: e.target.value,
-    }));
-  }, []);
-
-  const existPreference = {
-    destinationAppFolder: localStorage.getItem("appPath"),
-    configFolder: localStorage.getItem("rootDirMachines"),
-  };
-
-  const [listFormInput, setListFormInput] = useState(existPreference);
 
   return (
     <div className={styles.container}>
@@ -103,42 +23,15 @@ function PageConfig() {
           {getTransition("errorElectronAPI")}
         </p>
       )}
-      <form className={styles.form} onSubmit={handleSubmit}>
-        {getDefaultListFormInput(handleChangeInput).map(
-          ({ type, id, onChange, btnGroup }) => {
-            const { type: dialogType } = btnGroup;
-            return (
-              <InputBrowserFolder
-                btnGroup={{
-                  type: dialogType,
-                  label: getTransition("choose"),
-                }}
-                id={id}
-                label={getTransition(id)}
-                onChange={onChange}
-                onClick={handleOpenFileDialog({ dialogId: id, dialogType })}
-                type={type}
-                value={listFormInput[id]}
-                key={id}
-              />
-            );
-          }
-        )}
+      <ConfigFormContainer className={styles.form}>
         <MessageContainer />
         <div className={styles.control}>
           <Button isPrimary disabled={!window.electronAPI} type="submit">
             {getTransition("save")}
           </Button>
-          <Button onClick={handleCancel} type="button">
-            {getTransition("cancel")}
-          </Button>
+          <CancelContainer />
         </div>
-        {errorMsg && (
-          <p className={styles.alert} role="alert">
-            {errorMsg}
-          </p>
-        )}
-      </form>
+      </ConfigFormContainer>
       <div className={styles.language}>
         <p className={styles.label}>{getTransition("changeLanguage")}</p>
         <LanguageContainer className={styles.language_select} />
